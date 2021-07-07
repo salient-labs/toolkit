4a77c4468c79dbe6afa8642d852fe326c9344f3e
Luke Arms <luke@linacreative.com>
2021-07-07 14:02:14 +1000

    Add Cli classes

---
+++ src/Cli/Cli.php
@@ -0,0 +1,341 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lkrms\Cli;
+use Lkrms\Util\Assert;
+use Lkrms\Util\Convert;
+use Exception;
+
+class Cli
+{
+    public const OPTION_TYPE_FLAG = 1 << 0;
+
+    public const OPTION_TYPE_VALUE = 1 << 1;
+
+    public const OPTION_TYPE_ONE_OF = 1 << 2;
+
+    public const OPTION_REQUIRED = 1 << 3;
+
+    public const OPTION_VALUE_NOT_REQUIRED = 1 << 4;
+
+    public const OPTION_MULTIPLE_ALLOWED = 1 << 5;
+
+    public const MASK_OPTION_TYPE = (1 << 3) - 1;
+
+    private static $Options = [];
+
+    private static $UniqueOptions = [];
+
+    private static $UsageShortFlags = [];
+
+    private static $UsageLongFlags = [];
+
+    private static $UsageRequiredValues = [];
+
+    private static $UsageOptionalValues = [];
+
+    private static $UsageArguments = [];
+
+    private static $GetOpt;
+
+    private static $GetOptCalled = false;
+
+    private static $NextArgumentIndex;
+
+    public static function ErrorWrite(string $string)
+    {
+        fwrite(STDERR, $string . "\n");
+    }
+
+    public static function GetCommandName()
+    {
+        return basename($GLOBALS['argv'][0]);
+    }
+
+    public static function AddOption( ? string $long, ? string $short, ? string $valueName, ? string $description, int $flags = self::OPTION_TYPE_FLAG, array $allowedValues = null, $defaultValue = null)
+    {
+        self::AddCliOption( new CliOption($long, $short, $valueName, $description, $flags, $allowedValues, $defaultValue));
+    }
+
+    public static function AddCliOption(CliOption $option)
+    {
+        if (self::$GetOptCalled)
+        {
+            throw new Exception('Cannot add options after calling GetOpt');
+        }
+
+        $hasShort     = ! is_null($option->Short);
+        $hasLong      = ! is_null($option->Long);
+        $optionNames  = [];
+
+        if ($hasShort)
+        {
+            Assert::ExactStringLength($option->Short, 1, 'option->Short');
+            $optionNames[] = $option->Short;
+        }
+
+        if ($hasLong)
+        {
+            Assert::MinimumStringLength($option->Long, 2, 'option->Long');
+            $optionNames[] = $option->Long;
+        }
+
+        if ( ! count($optionNames))
+        {
+            throw new Exception('Option name missing');
+        }
+
+        if (count(array_intersect($optionNames, array_keys(self::$Options))))
+        {
+            throw new Exception('Option already defined');
+        }
+
+        self::$UniqueOptions[$option->Key] = $option;
+
+        if ($hasShort)
+        {
+            self::$Options[$option->Short] = $option;
+        }
+
+        if ($hasLong)
+        {
+            self::$Options[$option->Long] = $option;
+        }
+
+        $usage = [];
+
+        if ($option->IsFlag)
+        {
+            if ($hasShort)
+            {
+                $usage[] = '-' . $option->Short;
+
+                // for abbreviated output
+                self::$UsageShortFlags[] = $option->Short;
+            }
+
+            if ($hasLong)
+            {
+                if ( ! $hasShort)
+                {
+                    self::$UsageLongFlags[] = $option->Long;
+                }
+
+                $usage[] = '--' . $option->Long;
+            }
+        }
+        else
+        {
+            $valueName    = $option->ValueName??'value';
+            $shortSuffix  = '';
+
+            if ($hasShort)
+            {
+                $usage[]      = "-{$option->Short}";
+                $shortSuffix  = $option->IsValueRequired ? " $valueName" : "[$valueName]";
+            }
+
+            if ($hasLong)
+            {
+                $usage[] = "--{$option->Long}" . ($option->IsValueRequired ? " $valueName" : "[=$valueName]");
+            }
+
+            if ($option->IsRequired)
+            {
+                self::$UsageRequiredValues[] = $usage[0] . $shortSuffix;
+            }
+            else
+            {
+                self::$UsageOptionalValues[] = $usage[0] . $shortSuffix;
+            }
+        }
+
+        self::$UsageArguments[] = [
+            'argument'      => implode(', ', $usage),
+            'description'   => $option->Description,
+            'defaultValue'  => $option->IsFlag ? null : (is_string($option->DefaultValue) || is_null($option->DefaultValue) ? $option->DefaultValue : json_encode($option->DefaultValue)),
+            'allowedValues' => $option->AllowedValues,
+        ];
+    }
+
+    public static function Usage(int $status = 0) : void
+    {
+        $usage = 'Usage: ' . self::GetCommandName();
+
+        if ( ! empty(self::$UsageShortFlags))
+        {
+            $usage .= ' [-' . implode('', self::$UsageShortFlags) . ']';
+        }
+
+        if ( ! empty(self::$UsageLongFlags))
+        {
+            $usage .= ' [--' . implode('] [--', self::$UsageLongFlags) . ']';
+        }
+
+        if ( ! empty(self::$UsageOptionalValues))
+        {
+            $usage .= ' [' . implode('] [', self::$UsageOptionalValues) . ']';
+        }
+
+        if ( ! empty(self::$UsageRequiredValues))
+        {
+            $usage .= ' ' . implode(' ', self::$UsageRequiredValues);
+        }
+
+        foreach (self::$UsageArguments as $option)
+        {
+            $usage .= "\n\n  $option[argument]";
+            $usage .= $option['description'] ? "\n    $option[description]" : '';
+            $usage .= $option['allowedValues'] ? "\n    Available values: " . implode(' ', $option['allowedValues']) : '';
+            $usage .= $option['defaultValue'] ? "\n    Default: $option[defaultValue]" : '';
+        }
+
+        self::ErrorWrite($usage);
+        exit ($status);
+    }
+
+    public static function GetOpt()
+    {
+        if ( ! self::$GetOptCalled)
+        {
+            self::$GetOptCalled = true;
+
+            // build arguments to getopt
+            $short  = '';
+            $long   = [];
+
+            foreach (self::$UniqueOptions as $option)
+            {
+                $suffix = '';
+
+                if ($option->IsValueRequired)
+                {
+                    $suffix = ':';
+                }
+                elseif ( ! $option->IsFlag)
+                {
+                    $suffix = '::';
+                }
+
+                if ($option->Short)
+                {
+                    $short .= $option->Short . $suffix;
+                }
+
+                if ($option->Long)
+                {
+                    $long[] = $option->Long . $suffix;
+                }
+            }
+
+            $opt = getopt($short, $long, self::$NextArgumentIndex);
+
+            if ($opt === false)
+            {
+                self::Usage(1);
+            }
+
+            $mergedOpt  = [];
+            $isHelp     = false;
+
+            foreach ($opt as $o => $a)
+            {
+                $o = self::$Options[$o]->Key??null;
+
+                if (is_null($o))
+                {
+                    throw new Exception('Unknown option returned by getopt');
+                }
+
+                if (isset($mergedOpt[$o]))
+                {
+                    $mergedOpt[$o] = array_merge(Convert::AnyToArray($mergedOpt[$o]), Convert::AnyToArray($a));
+                }
+                else
+                {
+                    $mergedOpt[$o] = $a;
+                }
+            }
+
+            foreach ($mergedOpt as $o => $a)
+            {
+                $option = self::$UniqueOptions[$o];
+
+                if ( ! $option->MultipleAllowed && is_array($a))
+                {
+                    self::ErrorWrite(self::GetCommandName() . ": {$option->DisplayName} cannot be used multiple times");
+                    $opt = false;
+                }
+
+                if ( ! is_null($option->AllowedValues))
+                {
+                    $arr = Convert::AnyToArray($a);
+
+                    foreach ($arr as $v)
+                    {
+                        if ( ! in_array($v, $option->AllowedValues))
+                        {
+                            self::ErrorWrite(self::GetCommandName() . ": invalid value for {$option->DisplayName} -- '$v'");
+                            $opt = false;
+                        }
+                    }
+                }
+            }
+
+            $isHelp = count(array_intersect(array_keys($mergedOpt), [
+                'h|help',
+                '|help'
+            ]));
+
+            foreach (self::$UniqueOptions as $o)
+            {
+                if ($o->IsRequired && ! isset($mergedOpt[$o->Key]))
+                {
+                    if ($GLOBALS['argc'] > 1 && ! $isHelp)
+                    {
+                        self::ErrorWrite(self::GetCommandName() . ": {$o->DisplayName} is required");
+                    }
+
+                    $opt = false;
+                }
+            }
+
+            if ($opt === false)
+            {
+                self::Usage($isHelp ? 0 : 1);
+            }
+
+            self::$GetOpt = $mergedOpt;
+        }
+
+        return self::$GetOpt;
+    }
+
+    public static function GetOptionValue(string $optionName)
+    {
+        if ( ! isset(self::$Options[$optionName]))
+        {
+            throw new Exception('Option not defined');
+        }
+
+        $option  = self::$Options[$optionName];
+        $opt     = self::GetOpt();
+        $val     = $opt[$option->Key]??$option->DefaultValue;
+
+        if ($option->IsFlag)
+        {
+            // setting a flag should make its value true
+            return is_array($val) ? count($val) : ! $val;
+        }
+        elseif ($option->MultipleAllowed)
+        {
+            return Convert::AnyToArray($val);
+        }
+        else
+        {
+            return $val;
+        }
+    }
+}
+
---
+++ src/Cli/CliOption.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lkrms\Cli;
+use Lkrms\Util\Assert;
+use Lkrms\Util\Convert;
+use Lkrms\Util\Test;
+use Exception;
+
+class CliOption
+{
+    public $Long;
+
+    public $Short;
+
+    public $Key;
+
+    public $DisplayName;
+
+    public $IsFlag;
+
+    public $IsRequired;
+
+    public $IsValueRequired;
+
+    public $MultipleAllowed;
+
+    public $ValueName;
+
+    public $Description;
+
+    public $AllowedValues;
+
+    public $DefaultValue;
+
+    private static function ScalarToString(mixed $value, string $errorMessage) : string
+    {
+        if (is_scalar($value))
+        {
+            return (string)$value;
+        }
+        else
+        {
+            throw new Exception($errorMessage);
+        }
+    }
+
+    public function __construct( ? string $long, ? string $short, ? string $valueName, ? string $description, int $flags = Cli::OPTION_TYPE_FLAG, array $allowedValues = null, $defaultValue = null)
+    {
+        if ( ! Test::IsOneFlagSet($flags, Cli::MASK_OPTION_TYPE))
+        {
+            throw new Exception('Invalid option type');
+        }
+
+        $this->Long   = Convert::EmptyToNull($long);
+        $this->Short  = Convert::EmptyToNull($short);
+
+        if ( ! is_null($this->Long))
+        {
+            Assert::PregMatch($long, '/^[a-z0-9][-a-z0-9_]+$/i', 'long');
+        }
+
+        if ( ! is_null($this->Short))
+        {
+            Assert::PregMatch($short, '/^[a-z0-9]$/i', 'short');
+        }
+
+        $this->Key              = $this->Short . '|' . $this->Long;
+        $this->DisplayName      = $this->Long ? '--' . $this->Long : ($this->Short ? '-' . $this->Short : null);
+        $this->IsFlag           = Test::IsFlagSet($flags, Cli::OPTION_TYPE_FLAG);
+        $this->IsRequired       = $this->IsFlag ? false : Test::IsFlagSet($flags, Cli::OPTION_REQUIRED);
+        $this->IsValueRequired  = $this->IsFlag ? false : ! Test::IsFlagSet($flags, Cli::OPTION_VALUE_NOT_REQUIRED);
+        $this->MultipleAllowed  = Test::IsFlagSet($flags, Cli::OPTION_MULTIPLE_ALLOWED);
+        $this->ValueName        = $this->IsFlag ? null : $valueName;
+        $this->Description      = $description;
+        $this->DefaultValue     = $this->IsFlag ? true : ($this->IsRequired ? null : $defaultValue);
+
+        if ( ! is_null($this->DefaultValue) && ! $this->IsFlag)
+        {
+            if ($this->MultipleAllowed)
+            {
+                $this->DefaultValue = Convert::AnyToArray($this->DefaultValue);
+                array_walk($this->DefaultValue,
+
+                function ( & $value)
+                {
+                    $value = self::ScalarToString($value, "defaultValue must be a scalar or an array of scalars");
+                }
+
+                );
+            }
+            else
+            {
+                $this->DefaultValue = self::ScalarToString($this->DefaultValue, "defaultValue must be a scalar");
+            }
+        }
+
+        if (Test::IsFlagSet($flags, Cli::OPTION_TYPE_ONE_OF))
+        {
+            Assert::NotEmpty($allowedValues, 'allowedValues');
+            $this->AllowedValues = $allowedValues;
+        }
+    }
+}
+
