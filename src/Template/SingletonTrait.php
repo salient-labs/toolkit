f4d6e1284c1188fadc519825409b7ff42a4d0595
Luke Arms <luke@linacreative.com>
2022-03-15 11:00:45 +1100

    Add Singleton (abstract class) and SingletonTrait
    Traits and interfaces currently in `Lkrms\Mixin` will be moved to
    `Lkrms\Template` in subsequent commits

---
+++ src/Template/SingletonTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lkrms\Template;
+
+/**
+ * Implements the singleton pattern
+ *
+ * @package Lkrms
+ * @see Singleton
+ */
+trait SingletonTrait
+{
+    private static $Singletons = [];
+
+    final protected function __construct()
+    {
+        self::$Singletons[ static::class] = $this;
+    }
+
+    final protected static function HasInstance(): bool
+    {
+        return !is_null(self::$Singletons[ static::class] ?? null);
+    }
+
+    /**
+     *
+     * @return static
+     */
+    public static function GetInstance()
+    {
+        return (self::$Singletons[ static::class] ?? null) ?: new static();
+    }
+}
+
