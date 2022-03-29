eca243a24a4d791c69e9ac51d66feeedcfee0f62
Luke Arms <luke@linacreative.com>
2022-03-29 11:14:06 +1100

    Remove SyncProvider::getCapabilities(), add HttpSyncProvider class

---
+++ src/Sync/Provider/HttpSyncProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lkrms\Sync\Provider;
+
+use Lkrms\Curler\CachingCurler;
+use Lkrms\Curler\Curler;
+use Lkrms\Curler\CurlerHeaders;
+use Lkrms\Sync\SyncProvider;
+
+/**
+ * Base class for HTTP-based RESTful API providers
+ *
+ * @package Lkrms
+ */
+abstract class HttpSyncProvider extends SyncProvider
+{
+    abstract protected function getBaseUrl(): string;
+
+    abstract protected function getHeaders(): CurlerHeaders;
+
+    protected function headersCallback(array $headers): array
+    {
+        return $this->getBackendIdentifier();
+    }
+
+    final public function getCurler(string $path, int $expiry = null): Curler
+    {
+        if (func_num_args() > 1)
+        {
+            return new CachingCurler(
+                $this->getBaseUrl() . $path,
+                $this->getHeaders(),
+                $expiry,
+                [$this, "headersCallback"]
+            );
+        }
+        else
+        {
+            return new Curler(
+                $this->getBaseUrl() . $path,
+                $this->getHeaders()
+            );
+        }
+    }
+}
+
--- src/Sync/SyncProvider.php
+++ src/Sync/SyncProvider.php
@@ -4,18 +4,22 @@ declare(strict_types=1);
 
 namespace Lkrms\Sync;
 
+use Lkrms\Generate;
+
 /**
+ * Base class for API providers
  *
  * @package Lkrms
  */
 abstract class SyncProvider
 {
     /**
-     * Returns a stable value unique to the connected backend instance
+     * Returns a stable identifier unique to the connected backend instance
      *
-     * The value returned should be the canonical form of the smallest possible
+     * This method must be idempotent for each backend instance the provider
+     * connects to. The return value should correspond to the smallest possible
      * set of stable metadata that uniquely identifies the specific data source
-     * the provider is currently connected to.
+     * backing the connected instance.
      *
      * This could include:
      * - an endpoint URI (if backend instances are URI-specific or can be
@@ -23,31 +27,25 @@ abstract class SyncProvider
      * - a tenant ID
      * - an installation GUID
      *
-     * It must NOT include:
-     * - unstable values like usernames, tokens and other identifiers with a
-     *   shorter lifespan than the data source itself
+     * It should not include:
+     * - usernames, API keys, tokens, or other identifiers with a shorter
+     *   lifespan than the data source itself
      * - values that aren't unique to the connected data source
      * - case-insensitive values (unless normalised first)
      *
-     * @return string|string[]
+     * @return string[]
      */
-    abstract public function getBackendIdentifier(): string;
+    abstract protected function getBackendIdentifier(): array;
 
     /**
-     * Builds a capability map based on the provider's public methods
+     * Returns a stable hash unique to the connected backend instance
      *
-     * Returns an array where each {@see SyncOperation} implemented by the
-     * provider appears in a list of capabilities for the associated
-     * {@see SyncEntity}.
-     *
-     * @return array<string,int[]> A map from entity names to capability lists.
+     * @return string
+     * @see SyncProvider::getBackendIdentifier()
      */
-    public function getCapabilities(): array
+    final public function getBackendHash(): string
     {
-        /**
-         * @todo Implement this
-         */
-        return [];
+        return Generate::hash(...$this->getBackendIdentifier());
     }
 }
 
