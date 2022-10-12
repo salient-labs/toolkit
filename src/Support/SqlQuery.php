1995ec5e27c19c679fd5f0f7300e3b2c9f7bf703
Luke Arms <luke@linacreative.com>
2022-10-13 01:43:56 +1100

    Make SqlQuery's interface (mostly) fluent
    - Add `where()`
    - Move `Sql::valueInList()` to `SqlQuery::whereValueInList()`
    - Remove `Sql` class and facade

--- src/Support/SqlQuery.php
+++ src/Support/SqlQuery.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Lkrms\Support;
 
+use Lkrms\Concept\FluentInterface;
 use Lkrms\Concern\TReadable;
 use Lkrms\Contract\IReadable;
 use UnexpectedValueException;
@@ -13,7 +14,7 @@ use UnexpectedValueException;
  *
  * @property-read array<string,mixed> $Values Parameter name => value
  */
-class SqlQuery implements IReadable
+class SqlQuery extends FluentInterface implements IReadable
 {
     use TReadable;
 
@@ -81,18 +82,77 @@ class SqlQuery implements IReadable
     }
 
     /**
-     * Add a parameter and return a query placeholder for it
+     * Add a parameter and assign its query placeholder to a variable
      *
+     * @return $this
      */
-    public function addParam(string $name, $value): string
+    public function addParam(string $name, $value, ?string & $placeholder)
     {
         if (array_key_exists($name, $this->Values))
         {
             throw new UnexpectedValueException("Parameter already added: $name");
         }
-        $_name = ($this->ParamCallback)($name);
+
+        $placeholder         = ($this->ParamCallback)($name);
         $this->Values[$name] = $value;
-        return $_name;
+
+        return $this;
+    }
+
+    /**
+     * Add a WHERE clause
+     *
+     * See {@see SqlQuery::$Where}.
+     *
+     * @param callable|string|array $clause A string, an array, or a callback
+     * that returns a string or array.
+     * ```php
+     * fn(): string|array
+     * ```
+     * @return $this
+     */
+    public function where($clause)
+    {
+        $this->Where[] = is_callable($clause) ? $clause() : $clause;
+
+        return $this;
+    }
+
+    /**
+     * Add "<name> IN (<value>[,<value>])" to the query unless a list of values
+     * is empty
+     *
+     * @return $this
+     */
+    public function whereValueInList(string $name, ...$value)
+    {
+        if (!count($value))
+        {
+            return $this;
+        }
+
+        $expr = [];
+        foreach ($value as $_value)
+        {
+            $expr[] = $this->addNextParam($_value);
+        }
+        $this->Where[] = "$name IN (" . implode(",", $expr) . ")";
+
+        return $this;
+    }
+
+    public function getWhere(?array & $values = null): ?string
+    {
+        $values = $this->Values;
+
+        return $this->buildWhere($this->Where) ?: null;
+    }
+
+    private function addNextParam($value): string
+    {
+        $this->addParam("param_" . count($this->Values), $value, $param);
+
+        return $param;
     }
 
     private function buildWhere(array $where)
@@ -111,15 +171,8 @@ class SqlQuery implements IReadable
                 $where[$i] = "($clause)";
             }
         }
-        return implode(" $glue ", $where);
-    }
 
-    /**
-     * @return string|null
-     */
-    public function getWhere(): ?string
-    {
-        return $this->buildWhere($this->Where) ?: null;
+        return implode(" $glue ", $where);
     }
 
 }
