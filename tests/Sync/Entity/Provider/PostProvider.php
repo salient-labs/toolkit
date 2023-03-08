3bffee2c1af64ad497529b376690443d7605dbe5
Luke Arms <luke@linacreative.com>
2023-03-08 11:05:06 +1100

    Update tests

--- tests/Sync/Entity/Provider/PostProvider.php
+++ tests/Sync/Entity/Provider/PostProvider.php
@@ -2,18 +2,18 @@
 
 namespace Lkrms\Tests\Sync\Entity\Provider;
 
+use Lkrms\Sync\Contract\ISyncContext;
 use Lkrms\Sync\Contract\ISyncProvider;
-use Lkrms\Sync\Support\SyncContext;
 use Lkrms\Tests\Sync\Entity\Post;
 
 /**
  * Syncs Post objects with a backend
  *
- * @method Post createPost(SyncContext $ctx, Post $post)
- * @method Post getPost(SyncContext $ctx, int|string|null $id)
- * @method Post updatePost(SyncContext $ctx, Post $post)
- * @method Post deletePost(SyncContext $ctx, Post $post)
- * @method iterable<Post> getPosts(SyncContext $ctx)
+ * @method Post createPost(ISyncContext $ctx, Post $post)
+ * @method Post getPost(ISyncContext $ctx, int|string|null $id)
+ * @method Post updatePost(ISyncContext $ctx, Post $post)
+ * @method Post deletePost(ISyncContext $ctx, Post $post)
+ * @method iterable<Post> getPosts(ISyncContext $ctx)
  *
  * @lkrms-generate-command lk-util generate sync provider --magic --op='create,get,update,delete,get-list' 'Lkrms\Tests\Sync\Entity\Post'
  */
--- tests/Sync/Entity/Provider/UserProvider.php
+++ tests/Sync/Entity/Provider/UserProvider.php
@@ -2,18 +2,18 @@
 
 namespace Lkrms\Tests\Sync\Entity\Provider;
 
+use Lkrms\Sync\Contract\ISyncContext;
 use Lkrms\Sync\Contract\ISyncProvider;
-use Lkrms\Sync\Support\SyncContext;
 use Lkrms\Tests\Sync\Entity\User;
 
 /**
  * Syncs User objects with a backend
  *
- * @method User createUser(SyncContext $ctx, User $user)
- * @method User getUser(SyncContext $ctx, int|string|null $id)
- * @method User updateUser(SyncContext $ctx, User $user)
- * @method User deleteUser(SyncContext $ctx, User $user)
- * @method iterable<User> getUsers(SyncContext $ctx)
+ * @method User createUser(ISyncContext $ctx, User $user)
+ * @method User getUser(ISyncContext $ctx, int|string|null $id)
+ * @method User updateUser(ISyncContext $ctx, User $user)
+ * @method User deleteUser(ISyncContext $ctx, User $user)
+ * @method iterable<User> getUsers(ISyncContext $ctx)
  *
  * @lkrms-generate-command lk-util generate sync provider --magic --op='create,get,update,delete,get-list' 'Lkrms\Tests\Sync\Entity\User'
  */
