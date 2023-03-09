eedcd22207317c739fc902f955c55d9939409ef4
Luke Arms <luke@linacreative.com>
2023-03-09 21:37:34 +1100

    Rearrange methods in `HttpSyncProvider`

--- src/Sync/Concept/HttpSyncProvider.php
+++ src/Sync/Concept/HttpSyncProvider.php
@@ -20,6 +20,109 @@ use Lkrms\Sync\Support\HttpSyncDefinitionBuilder;
 abstract class HttpSyncProvider extends SyncProvider
 {
     /**
+     * Get a Curler instance bound to an API endpoint
+     *
+     * If `$expiry` is an integer less than `0` (the default), it is replaced
+     * with the return value of {@see HttpSyncProvider::getExpiry()}.
+     *
+     * If set, `$headers` and `$pager` take precedence over values applied via
+     * {@see HttpSyncProvider::buildCurler()}, which, in turn, take precedence
+     * over {@see HttpSyncProvider::getHeaders()} and
+     * {@see HttpSyncProvider::getPager()}.
+     *
+     */
+    final public function getCurler(string $path, ?int $expiry = -1, ?ICurlerHeaders $headers = null, ?ICurlerPager $pager = null): Curler
+    {
+        $curlerB = $this->buildCurler(CurlerBuilder::build());
+
+        if (!is_null($expiry) && $expiry < 0) {
+            $expiry = $this->getExpiry($path);
+        }
+        if (!is_null($expiry)) {
+            $curlerB = $curlerB->cacheResponse()
+                               ->expiry($expiry);
+        } else {
+            $curlerB = $curlerB->cacheResponse(false);
+        }
+
+        if ($headers) {
+            $curlerB = $curlerB->headers($headers);
+        } elseif (!$curlerB->isset('headers')) {
+            $curlerB = $curlerB->headers($this->getHeaders($path));
+        }
+
+        if ($pager) {
+            $curlerB = $curlerB->pager($pager);
+        } elseif (!$curlerB->isset('pager')) {
+            $curlerB = $curlerB->pager($this->getPager($path));
+        }
+
+        return
+            $curlerB->baseUrl($this->getEndpointUrl($path))
+                    ->go();
+    }
+
+    final public function getDefinition(string $entity): ISyncDefinition
+    {
+        return
+            $this->getHttpDefinition(
+                     $entity,
+                     HttpSyncDefinition::build()
+                         ->entity($entity)
+                         ->provider($this)
+                 )
+                 ->go();
+    }
+
+    /**
+     * Get the URL of an API endpoint
+     *
+     */
+    final public function getEndpointUrl(string $path): string
+    {
+        return $this->getBaseUrl($path) . $path;
+    }
+
+    public function checkHeartbeat(int $ttl = 300)
+    {
+        throw new MethodNotImplementedException(
+            static::class,
+            __FUNCTION__,
+            IProvider::class
+        );
+    }
+
+    /**
+     * Configure an unresolved Curler instance for upstream requests
+     *
+     * Called once per {@see HttpSyncProvider::getCurler()} call.
+     *
+     * {@see HttpSyncProvider::getHeaders()} and
+     * {@see HttpSyncProvider::getPager()} are not called if
+     * {@see HttpSyncProvider::buildCurler()} sets their respective properties
+     * via {@see CurlerBuilder::headers()} or {@see CurlerBuilder::pager()}.
+     *
+     */
+    protected function buildCurler(CurlerBuilder $curlerB): CurlerBuilder
+    {
+        return $curlerB;
+    }
+
+    /**
+     * Surface the provider's implementation of sync operations for an entity
+     * via an HttpSyncDefinition object
+     *
+     * Return `$defB` if no sync operations are implemented for the entity.
+     *
+     * @param HttpSyncDefinitionBuilder $defB A definition builder with
+     * `entity()` and `provider()` already applied.
+     */
+    protected function getHttpDefinition(string $entity, HttpSyncDefinitionBuilder $defB): HttpSyncDefinitionBuilder
+    {
+        return $defB;
+    }
+
+    /**
      * Get the base URL of the upstream API
      *
      * `$path` should be ignored unless the provider uses endpoint-specific base
@@ -61,104 +164,6 @@ abstract class HttpSyncProvider extends SyncProvider
     protected function getExpiry(?string $path): ?int
     {
         return null;
-    }
-
-    /**
-     * Configure an unresolved Curler instance for upstream requests
-     *
-     * Called once per {@see HttpSyncProvider::getCurler()} call.
-     *
-     * {@see HttpSyncProvider::getHeaders()} and
-     * {@see HttpSyncProvider::getPager()} are not called if
-     * {@see HttpSyncProvider::buildCurler()} sets their respective properties
-     * via {@see CurlerBuilder::headers()} or {@see CurlerBuilder::pager()}.
-     *
-     */
-    protected function buildCurler(CurlerBuilder $curlerB): CurlerBuilder
-    {
-        return $curlerB;
-    }
-
-    /**
-     * Surface the provider's implementation of sync operations for an entity
-     * via an HttpSyncDefinition object
-     *
-     * Return `$defB` if no sync operations are implemented for the entity.
-     *
-     * @param HttpSyncDefinitionBuilder $defB A definition builder with
-     * `entity()` and `provider()` already applied.
-     */
-    protected function getHttpDefinition(string $entity, HttpSyncDefinitionBuilder $defB): HttpSyncDefinitionBuilder
-    {
-        return $defB;
-    }
-
-    final public function getDefinition(string $entity): ISyncDefinition
-    {
-        return
-            $this->getHttpDefinition(
-                     $entity,
-                     HttpSyncDefinition::build()
-                         ->entity($entity)
-                         ->provider($this)
-                 )
-                 ->go();
-    }
-
-    /**
-     * Get the URL of an API endpoint
-     *
-     */
-    final public function getEndpointUrl(string $path): string
-    {
-        return $this->getBaseUrl($path) . $path;
-    }
-
-    /**
-     * Get a Curler instance bound to an API endpoint
-     *
-     * If `$expiry` is an integer less than `0`, the return value of
-     * {@see HttpSyncProvider::getExpiry()} is used as the response expiry time.
-     *
-     */
-    final public function getCurler(string $path, ?int $expiry = -1, ?ICurlerHeaders $headers = null, ?ICurlerPager $pager = null): Curler
-    {
-        $curlerB = $this->buildCurler(CurlerBuilder::build());
-
-        if (!is_null($expiry) && $expiry < 0) {
-            $expiry = $this->getExpiry($path);
-        }
-        if (!is_null($expiry)) {
-            $curlerB = $curlerB->cacheResponse()
-                               ->expiry($expiry);
-        } else {
-            $curlerB = $curlerB->cacheResponse(false);
-        }
-
-        if ($headers) {
-            $curlerB = $curlerB->headers($headers);
-        } elseif (!$curlerB->isset('headers')) {
-            $curlerB = $curlerB->headers($this->getHeaders($path));
-        }
-
-        if ($pager) {
-            $curlerB = $curlerB->pager($pager);
-        } elseif (!$curlerB->isset('pager')) {
-            $curlerB = $curlerB->pager($this->getPager($path));
-        }
-
-        return
-            $curlerB->baseUrl($this->getEndpointUrl($path))
-                    ->go();
-    }
-
-    public function checkHeartbeat(int $ttl = 300)
-    {
-        throw new MethodNotImplementedException(
-            static::class,
-            __FUNCTION__,
-            IProvider::class
-        );
     }
 
     /**
