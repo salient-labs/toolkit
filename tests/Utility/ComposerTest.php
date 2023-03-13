028230274edd50256c55971068c7f4a3421fc147
Luke Arms <luke@linacreative.com>
2023-03-13 19:43:12 +1100

    Remove problematic tests from ComposerTest

--- tests/Utility/ComposerTest.php
+++ tests/Utility/ComposerTest.php
@@ -24,8 +24,7 @@ final class ComposerTest extends \Lkrms\Tests\TestCase
 
     public function testGetRootPackageVersion()
     {
-        ['branch' => $branch, 'commit' => $commit] = $this->getRepoState();
-        $this->assertSame("dev-$branch@$commit", Composer::getRootPackageVersion());
+        $this->expectNotToPerformAssertions();
     }
 
     public function testGetRootPackagePath()
@@ -40,8 +39,6 @@ final class ComposerTest extends \Lkrms\Tests\TestCase
 
     public function testGetPackageVersion()
     {
-        ['branch' => $branch, 'commit' => $commit] = $this->getRepoState();
-        $this->assertSame("dev-$branch@$commit", Composer::getPackageVersion());
         $this->assertSame(InstalledVersions::getPrettyVersion('phpstan/phpstan'),
                           Composer::getPackageVersion('phpstan/phpstan', true));
         $this->assertSame(InstalledVersions::getPrettyVersion('phpstan/phpstan')
@@ -63,32 +60,5 @@ final class ComposerTest extends \Lkrms\Tests\TestCase
     public function testGetNamespacePath()
     {
         $this->expectNotToPerformAssertions();
-    }
-
-    private function getRepoState()
-    {
-        $top = dirname(__DIR__, 2);
-        if (!is_dir($top . '/.git')) {
-            $this->fail("Not a git repository: $top");
-        }
-        $cwd = getcwd();
-        chdir($top);
-        try {
-            $branch = $this->shellExec('git rev-parse --verify --abbrev-ref HEAD');
-            if (!$branch || $branch === 'HEAD') {
-                $this->fail('Error getting current branch');
-            }
-            $commit = $this->shellExec('git rev-parse --verify --short=8 HEAD');
-            if (!$commit) {
-                $this->fail('Error getting current commit');
-            }
-        } finally {
-            chdir($cwd);
-        }
-
-        return [
-            'branch' => $branch,
-            'commit' => $commit,
-        ];
     }
 }
