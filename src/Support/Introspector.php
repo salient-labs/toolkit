salient-labs/toolkit

commit b4450263285f924abb82ec34e57c61dff2c41989

Fix issues reported by downstream projects
- Fix issue where entities yielded by generators fail to serialize
- Return `IIterable<entity>` from `IProvidable::provideList()`
- Add `IIterable::toArray()`
- Add optional `$clean` argument to `Conversions::linesToLists()`
- Add `Conversions::valueAtKey()`

`Curler`:
- Simplify `ICurlerPager::prepareQuery()` signature
- Rework `QueryPager` to improve versatility
- Allow `AlwaysPaginate` to be set with no `Pager`
- Only use a pager in `initialise()` if one is passed in, to prevent
  interference with requests when pagination is not being used

src/Support/Introspector.php
@@ -748,6 +748,10 @@ class Introspector
                 } else {
                     $arr[$key] = $instance->{$props[$key]};
                 }
+                // Iterators aren't serializable, so convert them to arrays
+                if (is_iterable($arr[$key]) && !is_array($arr[$key])) {
+                    $arr[$key] = iterator_to_array($arr[$key]);
+                }
             }
 
             return $arr;

src/Concern/TProvidable.php
@@ -2,10 +2,12 @@
 
 namespace Lkrms\Concern;
 
+use Lkrms\Contract\IIterable;
 use Lkrms\Contract\IProvider;
 use Lkrms\Contract\IProviderContext;
 use Lkrms\Support\ArrayKeyConformity;
 use Lkrms\Support\Introspector;
+use Lkrms\Support\IterableIterator;
 use Lkrms\Support\ProviderContext;
 use RuntimeException;
 
@@ -115,13 +117,29 @@ trait TProvidable
      *
      * @param iterable<array> $dataList
      * @phpstan-param ArrayKeyConformity::* $conformity
-     * @return iterable<static>
+     * @return IIterable<static>
      */
     final public static function provideList(
         iterable $dataList,
         IProvider $provider,
         int $conformity = ArrayKeyConformity::NONE,
         ?IProviderContext $context = null
+    ): IIterable {
+        return IterableIterator::from(
+            self::_provideList($dataList, $provider, $conformity, $context)
+        );
+    }
+
+    /**
+     * @param iterable<array> $dataList
+     * @phpstan-param ArrayKeyConformity::* $conformity
+     * @return iterable<static>
+     */
+    private static function _provideList(
+        iterable $dataList,
+        IProvider $provider,
+        int $conformity,
+        ?IProviderContext $context
     ): iterable {
         $container = ($context
             ? $context->container()

src/Contract/IIterable.php
@@ -11,6 +11,13 @@ namespace Lkrms\Contract;
 interface IIterable extends \Iterator
 {
     /**
+     * Convert the iterator's remaining elements to an array
+     *
+     * @return TValue[]
+     */
+    public function toArray(): array;
+
+    /**
      * Apply a callback to the iterator's remaining elements
      *
      * @param callable(TValue): mixed $callback

src/Contract/IProvidable.php
@@ -98,12 +98,12 @@ interface IProvidable extends ReceivesService, ReturnsService
 
     /**
      * @param iterable<array> $dataList
-     * @return iterable<static>
+     * @return IIterable<static>
      */
     public static function provideList(
         iterable $dataList,
         IProvider $provider,
         int $conformity = ArrayKeyConformity::NONE,
         ?IProviderContext $context = null
-    ): iterable;
+    ): IIterable;
 }

src/Curler/Contract/ICurlerPager.php
@@ -7,20 +7,25 @@ use Lkrms\Curler\Curler;
 /**
  * Implements retrieval and extraction of paginated data
  *
+ * Instances may be reused. If necessary, cleanup between requests should be
+ * performed in {@see ICurlerPager::prepareQuery()}.
+ *
+ * Inheritors may be expected to service requests for entities via
+ * {@see ICurlerPager::getPage()} in scenarios where paging is unnecessary
+ * and/or unsupported. This may require deferral of pagination-related checks
+ * until page 2 is requested.
  */
 interface ICurlerPager
 {
     /**
      * Prepare a query string for the first page request
      *
-     * Do not include a leading question mark (`?`) in the query string. This
-     * will be added by {@see Curler}.
-     *
-     * Return `null` if no special query handling is required.
+     * Return `$query` if no special handling is required.
      *
      * @param mixed[]|null $query
+     * @return mixed[]|null
      */
-    public function prepareQuery(?array $query): ?string;
+    public function prepareQuery(?array $query): ?array;
 
     /**
      * Prepare a Curler instance to request the first page from an endpoint

src/Curler/Curler.php
@@ -542,12 +542,12 @@ final class Curler implements IReadable, IWritable, HasBuilder
         $this->Handle = null;
     }
 
-    private function initialise(string $method, ?array $query): void
+    private function initialise(string $method, ?array $query, ?ICurlerPager $pager = null): void
     {
-        if ($this->Pager && !is_null($_query = $this->Pager->prepareQuery($query))) {
-            $_query = '?' . $_query;
+        if ($pager) {
+            $query = $pager->prepareQuery($query);
         }
-        $this->QueryString = ($_query ?? null) ?: $this->getQueryString($query);
+        $this->QueryString = $this->getQueryString($query);
 
         $this->createHandle($this->BaseUrl . $this->QueryString);
 
@@ -561,8 +561,8 @@ final class Curler implements IReadable, IWritable, HasBuilder
         $this->setContentType(null);
         $this->Body = $this->Data = null;
 
-        if ($this->Pager) {
-            $this->Pager->prepareCurler($this);
+        if ($pager) {
+            $pager->prepareCurler($this);
         }
 
         if ($this->ExpectJson) {
@@ -1006,16 +1006,14 @@ final class Curler implements IReadable, IWritable, HasBuilder
     private function process(string $method, ?array $query, $data = null, ?string $mimeType = null)
     {
         $isRaw = !is_null($mimeType);
-        if ($this->AlwaysPaginate && !$isRaw) {
-            if (!$this->Pager) {
-                throw new UnexpectedValueException(static::class . '::$Pager is not set');
-            }
+        if ($this->Pager && $this->AlwaysPaginate && !$isRaw) {
+            $pager = $this->Pager;
             if ($method !== HttpRequestMethod::GET) {
-                $data = $this->Pager->prepareData($data);
+                $data = $pager->prepareData($data);
             }
         }
 
-        $this->initialise($method, $query);
+        $this->initialise($method, $query, $pager ?? null);
 
         $this->Data = $data;
         if (is_array($data)) {
@@ -1034,8 +1032,8 @@ final class Curler implements IReadable, IWritable, HasBuilder
         if ($this->responseContentTypeIs(MimeType::JSON)) {
             $response = json_decode($this->ResponseBody, $this->ObjectAsArray);
 
-            if ($this->AlwaysPaginate) {
-                $response = $this->Pager->getPage($response, $this)->entities();
+            if ($pager ?? null) {
+                $response = $pager->getPage($response, $this)->entities();
                 if (array_keys($response) === [0] &&
                         (is_array($response[0]) || is_scalar($response[0]))) {
                     return $response[0];
@@ -1057,7 +1055,7 @@ final class Curler implements IReadable, IWritable, HasBuilder
             $data = $this->Pager->prepareData($data);
         }
 
-        $this->initialise($method, $query);
+        $this->initialise($method, $query, $this->Pager);
 
         do {
             if (!is_null($data)) {

src/Curler/Pager/ODataPager.php
@@ -30,9 +30,9 @@ final class ODataPager implements ICurlerPager
         $this->MaxPageSize = $maxPageSize;
     }
 
-    public function prepareQuery(?array $query): ?string
+    public function prepareQuery(?array $query): ?array
     {
-        return null;
+        return $query;
     }
 
     public function prepareData(?array $data): ?array

src/Curler/Pager/QueryPager.php
@@ -8,7 +8,6 @@ use Lkrms\Curler\Contract\ICurlerPager;
 use Lkrms\Curler\Curler;
 use Lkrms\Curler\Support\CurlerPageBuilder;
 use Lkrms\Facade\Convert;
-use LogicException;
 
 /**
  * Increments a value in the query string and repeats the request until no
@@ -17,35 +16,34 @@ use LogicException;
  */
 final class QueryPager implements ICurlerPager
 {
-    /**
-     * @var string|null
-     */
-    private $PageKey;
+    private ?string $PageKey;
+
+    private Closure $Selector;
+
+    private ?int $PageSize;
+
+    private array $Query;
+
+    private ?string $QueryPageKey;
 
     /**
-     * @var string|null
+     * @var true|null
      */
-    private $CurrentPageKey;
-
-    /**
-     * @var Closure
-     */
-    private $Selector;
-
-    /**
-     * @var array|null
-     */
-    private $Query;
+    private $QueryPageKeyChecked;
 
     /**
      * @param string|null $pageKey The value to increment with each request, or
-     * `null` to use the first value in the query.
+     * `null` to use the first value in the query. Added to the query string of
+     * the second and subsequent requests if missing from the first.
      * @param Closure|string|null $selector Entities are collected from:
      * - `<Selector>($response)` if `$selector` is a closure,
      * - `$response[<Selector>]` if `$selector` is a string, or
      * - The response itself
+     * @param int|null $pageSize Another page is requested if:
+     * - `$pageSize` is `null` and at least one result is returned, or
+     * - `$pageSize` is `>0` and exactly `$pageSize` results are returned
      */
-    public function __construct(?string $pageKey = null, $selector = null)
+    public function __construct(?string $pageKey = null, $selector = null, ?int $pageSize = null)
     {
         $this->PageKey = $pageKey;
         $this->Selector =
@@ -54,21 +52,29 @@ final class QueryPager implements ICurlerPager
                 : (is_string($selector)
                     ? fn($response) => $response[$selector]
                     : fn($response) => Convert::toList($response));
+        $this->PageSize = $pageSize;
     }
 
-    public function prepareQuery(?array $query): ?string
+    public function prepareQuery(?array $query): ?array
     {
-        if (is_null($this->PageKey)) {
-            if (is_null($query) ||
-                    !is_int(reset($query)) ||
-                    !($pageKey = key($query))) {
-                throw new LogicException('First element of query array must be an integer');
-            }
-            $this->CurrentPageKey = $pageKey;
-        }
-        $this->Query = $query;
+        // Save the query for subsequent requests
+        $this->Query = $query ?: [];
 
-        return null;
+        // Clear the last detected page key to ensure `getPage()` starts over
+        if (is_null($this->PageKey)) {
+            $this->QueryPageKey = null;
+            $this->QueryPageKeyChecked = null;
+
+            return $query;
+        }
+
+        // Or, if a page key has been set but doesn't appear in the query, add
+        // an initial value to `$this->Query` without changing the first request
+        if (!array_key_exists($this->PageKey, $this->Query)) {
+            $this->Query[$this->PageKey] = 1;
+        }
+
+        return $query;
     }
 
     public function prepareData(?array $data): ?array
@@ -85,9 +91,25 @@ final class QueryPager implements ICurlerPager
     {
         $data = ($this->Selector)($data);
 
-        if ($data) {
-            $this->Query[$this->CurrentPageKey ?: $this->PageKey]++;
-            $nextUrl = $curler->getQueryUrl($this->Query);
+        if ($data &&
+            (!$this->PageSize ||
+                $this->PageSize < 1 ||
+                count($data) === $this->PageSize)) {
+            $key = $this->PageKey;
+            if (is_null($key)) {
+                if ($this->QueryPageKeyChecked) {
+                    $key = $this->QueryPageKey;
+                } else {
+                    if ($this->Query && is_int(reset($this->Query))) {
+                        $key = $this->QueryPageKey = key($this->Query);
+                    }
+                    $this->QueryPageKeyChecked = true;
+                }
+            }
+            if (!is_null($key)) {
+                $this->Query[$key]++;
+                $nextUrl = $curler->getQueryUrl($this->Query);
+            }
         }
 
         return CurlerPageBuilder::build()

commit 3347bea3136e6d32f87251e608f18ecfb8869322

Fix serialization issue introduced in b4450263
- After converting an iterator to an array during entity serialization,
  replace the iterator with the array so the original entity isn't left
  with, say, an empty forward-only iterator

src/Support/Introspector.php
@@ -740,17 +740,21 @@ class Introspector
             sort($keys);
         }
 
-        $closure = (static function ($instance) use ($keys, $methods, $props) {
+        // Iterators aren't serializable, so they're converted to arrays
+        $resolveIterator = function (&$value): void {
+            if (is_iterable($value) && !is_array($value)) {
+                $value = iterator_to_array($value);
+            }
+        };
+        $closure = (static function ($instance) use ($keys, $methods, $props, $resolveIterator) {
             $arr = [];
             foreach ($keys as $key) {
                 if ($method = $methods[$key] ?? null) {
                     $arr[$key] = $instance->{$method}();
+                    $resolveIterator($arr[$key]);
                 } else {
+                    $resolveIterator($instance->{$props[$key]});
                     $arr[$key] = $instance->{$props[$key]};
-                }
-                // Iterators aren't serializable, so convert them to arrays
-                if (is_iterable($arr[$key]) && !is_array($arr[$key])) {
-                    $arr[$key] = iterator_to_array($arr[$key]);
                 }
             }
 