ee735ed216a69d6e7efa82339b3bc5c7fa5679d2
Luke Arms <luke@linacreative.com>
2023-06-12 23:38:36 +1000

    Improve portability of system timer test

--- tests/Utility/SystemTest.php
+++ tests/Utility/SystemTest.php
@@ -14,31 +14,33 @@ final class SystemTest extends \Lkrms\Tests\TestCase
         $time0 = hrtime(true) / 1000000;
         $system->startTimer('primary');
         $system->startTimer('secondary');
-        usleep(25000);
+        usleep(100000);
         $time1 = hrtime(true) / 1000000;
 
         $system->startTimer('other', 'special');
         $secondary = $system->stopTimer('secondary');
-        usleep(50000);
+        usleep(200000);
         $time2 = hrtime(true) / 1000000;
 
         $system->startTimer('secondary');
-        usleep(10000);
+        usleep(75000);
         $time3 = hrtime(true) / 1000000;
 
         $system->stopTimer('secondary');
         $system->stopTimer('other', 'special');
-        usleep(25000);
+        usleep(100000);
         $time4 = hrtime(true) / 1000000;
 
         $timers = $system->getTimers();
         $generalTimers = $system->getTimers(true, 'general');
         $specialTimers = $system->getTimers(true, 'special');
         $stoppedTimers = $system->getTimers(false);
-        $this->assertEqualsWithDelta($time1 - $time0, $secondary, 1);
-        $this->assertEqualsWithDelta($time4 - $time0, $timers['general']['primary'][0], 1);
-        $this->assertEqualsWithDelta(($time1 - $time0) + ($time3 - $time2), $timers['general']['secondary'][0], 1);
-        $this->assertEqualsWithDelta($time3 - $time1, $timers['special']['other'][0], 1);
+        // Allow timing to be off by up to 16ms because of [Windows] timer
+        // granularity
+        $this->assertEqualsWithDelta($time1 - $time0, $secondary, 16);
+        $this->assertEqualsWithDelta($time4 - $time0, $timers['general']['primary'][0], 16);
+        $this->assertEqualsWithDelta(($time1 - $time0) + ($time3 - $time2), $timers['general']['secondary'][0], 16);
+        $this->assertEqualsWithDelta($time3 - $time1, $timers['special']['other'][0], 16);
         $this->assertSame(1, $timers['general']['primary'][1]);
         $this->assertSame(2, $timers['general']['secondary'][1]);
         $this->assertSame(1, $timers['special']['other'][1]);
