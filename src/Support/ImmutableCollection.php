f6292a9a197db495c55d1cd853925cff982f9791
Luke Arms <luke@linacreative.com>
2023-06-16 14:50:24 +1000

    Add `ImmutableCollection` (for completeness)

---
+++ src/Support/ImmutableCollection.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Lkrms\Support;
+
+use Lkrms\Concern\TImmutableCollection;
+use Lkrms\Contract\ICollection;
+use Lkrms\Contract\IImmutable;
+
+/**
+ * A strictly immutable collection of values
+ *
+ * @template TKey of array-key
+ * @template TValue
+ *
+ * @implements ICollection<TKey,TValue>
+ */
+final class ImmutableCollection implements ICollection, IImmutable
+{
+    /**
+     * @use TImmutableCollection<TKey,TValue>
+     */
+    use TImmutableCollection;
+}
