ec25f5b91b7aec54478b560cab367494e83429bf
Luke Arms <luke@linacreative.com>
2023-10-24 12:28:43 +1100

    Review `Computations`, add `ngramSimilarity()` and `ngrams()`, add tests

--- src/Utility/Computations.php
+++ src/Utility/Computations.php
@@ -11,7 +11,7 @@ use Lkrms\Utility\Convert;
 final class Computations
 {
     /**
-     * Generate a cryptographically secure random UUID
+     * Get a cryptographically secure UUID
      *
      * Of the 128 bits returned, 122 are random.
      *
@@ -20,7 +20,7 @@ final class Computations
      * @param bool $binary If `true`, 16 bytes of raw binary data are returned
      * instead of a 36-byte hexadecimal representation.
      */
-    public function uuid(bool $binary = false): string
+    public static function uuid(bool $binary = false): string
     {
         $uuid[] = random_bytes(4);
         $uuid[] = random_bytes(2);
@@ -38,9 +38,9 @@ final class Computations
     }
 
     /**
-     * Generate a cryptographically secure string
+     * Get a cryptographically secure string
      */
-    public function randomText(int $length, string $chars = Char::ALPHANUMERIC): string
+    public static function randomText(int $length, string $chars = Char::ALPHANUMERIC): string
     {
         $max = strlen($chars) - 1;
         $text = '';
@@ -51,40 +51,41 @@ final class Computations
     }
 
     /**
-     * Generate a unique non-crypto hash and return raw binary data
+     * Get a unique non-crypto hash in raw binary form
      *
      * @param int|float|string|bool|\Stringable|null ...$value
      */
-    public function binaryHash(...$value): string
+    public static function binaryHash(...$value): string
     {
         // xxHash isn't supported until PHP 8.1, so MD5 is the best fit
         return hash('md5', implode("\0", Convert::toStrings(...$value)), true);
     }
 
     /**
-     * Generate a unique non-crypto hash and return a hexadecimal string
+     * Get a unique non-crypto hash in hexadecimal form
      *
      * @param int|float|string|bool|\Stringable|null ...$value
      */
-    public function hash(...$value): string
+    public static function hash(...$value): string
     {
         return hash('md5', implode("\0", Convert::toStrings(...$value)));
     }
 
     /**
-     * Returns the Levenshtein distance between two strings relative to the
-     * length of the longest string
+     * Get the Levenshtein distance between two strings relative to the length
+     * of the longest string
      *
-     * @param string $string1
-     * @param string $string2
-     * @param bool $normalise If true, normalise the strings with
+     * @param bool $normalise If true, normalise `$string1` and `$string2` with
      * {@see Conversions::toNormal()} before comparing them.
      * @return float A value between `0` and `1`, where `0` means the strings
      * are identical, and `1` means they have no similarities.
      */
-    public function textDistance(string $string1, string $string2, bool $normalise = true): float
-    {
-        if ($string1 . $string2 == '') {
+    public static function textDistance(
+        string $string1,
+        string $string2,
+        bool $normalise = true
+    ): float {
+        if ($string1 === '' && $string2 === '') {
             return (float) 0;
         }
 
@@ -93,23 +94,26 @@ final class Computations
             $string2 = Convert::toNormal($string2);
         }
 
-        return levenshtein($string1, $string2) / max(strlen($string1), strlen($string2));
+        return
+            levenshtein($string1, $string2)
+            / max(strlen($string1), strlen($string2));
     }
 
     /**
-     * Returns the similarity of two strings relative to the length of the
-     * longest string
+     * Get the similarity of two strings relative to the length of the longest
+     * string
      *
-     * @param string $string1
-     * @param string $string2
-     * @param bool $normalise If true, normalise the strings with
+     * @param bool $normalise If true, normalise `$string1` and `$string2` with
      * {@see Conversions::toNormal()} before comparing them.
      * @return float A value between `0` and `1`, where `0` means the strings
      * have no similarities, and `1` means they are identical.
      */
-    public function textSimilarity(string $string1, string $string2, bool $normalise = true): float
-    {
-        if ($string1 . $string2 == '') {
+    public static function textSimilarity(
+        string $string1,
+        string $string2,
+        bool $normalise = true
+    ): float {
+        if ($string1 === '' && $string2 === '') {
             return (float) 1;
         }
 
@@ -118,6 +122,78 @@ final class Computations
             $string2 = Convert::toNormal($string2);
         }
 
-        return max(similar_text($string1, $string2), similar_text($string2, $string1)) / max(strlen($string1), strlen($string2));
+        return
+            max(
+                similar_text($string1, $string2),
+                similar_text($string2, $string1),
+            ) / max(
+                strlen($string1),
+                strlen($string2),
+            );
+    }
+
+    /**
+     * Get the ngrams shared between two strings relative to the number of
+     * ngrams in the longest string
+     *
+     * @param bool $normalise If true, normalise `$string1` and `$string2` with
+     * {@see Conversions::toNormal()} before comparing them.
+     * @return float A value between `0` and `1`, where `0` means the strings
+     * have no shared ngrams, and `1` means their ngrams are identical.
+     */
+    public static function ngramSimilarity(
+        string $string1,
+        string $string2,
+        bool $normalise = true,
+        int $size = 2
+    ): float {
+        if (strlen($string1) < $size && strlen($string2) < $size) {
+            return (float) 1;
+        }
+
+        if ($normalise) {
+            $string1 = Convert::toNormal($string1);
+            $string2 = Convert::toNormal($string2);
+        }
+
+        $ngrams1 = self::ngrams($string1, $size);
+        $ngrams2 = self::ngrams($string2, $size);
+        $count = max(count($ngrams1), count($ngrams2));
+
+        $same = 0;
+        foreach ($ngrams1 as $ngram) {
+            $key = array_search($ngram, $ngrams2, true);
+            if ($key !== false) {
+                $same++;
+                unset($ngrams2[$key]);
+            }
+        }
+
+        return $same / $count;
+    }
+
+    /**
+     * Get a string's n-grams
+     *
+     * @return string[]
+     */
+    public static function ngrams(string $string, int $size = 2): array
+    {
+        $ngrams = [];
+        for ($i = 0; $i < $size; $i++) {
+            $split = $i
+                ? substr($string, $i)
+                : $string;
+            $trim = strlen($split) % $size;
+            if ($trim) {
+                $split = substr($split, 0, -$trim);
+            }
+            if ($split === '') {
+                continue;
+            }
+            $ngrams = array_merge($ngrams, str_split($split, $size));
+        }
+
+        return $ngrams;
     }
 }
