f40ed10d4a9bd6044680654770efe57ba2f514ad
Luke Arms <luke@linacreative.com>
2023-10-26 15:16:58 +1100

    Update `.prettyphp` and formatting

--- stubs/PhpToken.php
+++ stubs/PhpToken.php
@@ -1,7 +1,5 @@
 <?php
 
 if (PHP_VERSION_ID < 80000 && extension_loaded('tokenizer')) {
-    class PhpToken extends Lkrms\Polyfill\PhpToken
-    {
-    }
+    class PhpToken extends Lkrms\Polyfill\PhpToken {}
 }
