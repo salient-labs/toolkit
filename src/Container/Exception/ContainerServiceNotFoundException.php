2def46781741a520653f25f9dd0a525beb4939fa
Luke Arms <luke@linacreative.com>
2023-12-13 01:57:47 +1100

    Update summaries of some exceptions
    [no ci]

--- src/Container/Exception/ContainerServiceNotFoundException.php
+++ src/Container/Exception/ContainerServiceNotFoundException.php
@@ -5,6 +5,6 @@ namespace Lkrms\Container\Exception;
 use Psr\Container\NotFoundExceptionInterface;
 
 /**
- * Thrown when a service container cannot resolve a service to an object
+ * Thrown when a container cannot resolve a service to an object
  */
 class ContainerServiceNotFoundException extends ContainerException implements NotFoundExceptionInterface {}
