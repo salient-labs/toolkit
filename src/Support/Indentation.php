2121ef64218b9d6a7d4a5297c9502c56002486dd
Luke Arms <luke@linacreative.com>
2023-12-29 15:27:25 +1100

    Add `File::guessIndentation()`

---
+++ src/Support/Indentation.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Lkrms\Support;
+
+final class Indentation
+{
+    /**
+     * @readonly
+     */
+    public bool $InsertSpaces;
+
+    /**
+     * @readonly
+     */
+    public int $TabSize;
+
+    public function __construct(bool $insertSpaces, int $tabSize)
+    {
+        $this->InsertSpaces = $insertSpaces;
+        $this->TabSize = $tabSize;
+    }
+}
