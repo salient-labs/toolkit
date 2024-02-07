d4813bb4150df7374006c293e36af640b1a6f808
Luke Arms <luke@linacreative.com>
2024-02-07 21:17:36 +1100

    Continue utility class cleanup
    - Move `ellipsize()`, `expandTabs()` and `expandLeadingTabs()` from
      `Convert` to `Str`
    - Move `Convert::linesToLists()` to `Str::mergeLists()`

--- tests/unit/Utility/StrTest.php
+++ tests/unit/Utility/StrTest.php
@@ -5,6 +5,7 @@ namespace Lkrms\Tests\Utility;
 use Lkrms\Tests\TestCase;
 use Lkrms\Utility\Get;
 use Lkrms\Utility\Str;
+use ReflectionParameter;
 
 final class StrTest extends TestCase
 {
@@ -242,6 +243,28 @@ final class StrTest extends TestCase
     }
 
     /**
+     * @dataProvider ellipsizeProvider
+     */
+    public function testEllipsize(string $expected, string $value, int $length): void
+    {
+        $this->assertSame($expected, Str::ellipsize($value, $length));
+    }
+
+    /**
+     * @return array<array{string,string,int}>
+     */
+    public static function ellipsizeProvider(): array
+    {
+        return [
+            ['Hello...', 'Hello, world!', 8],
+            ['Hello', 'Hello', 5],
+            ['...', 'Hello', 3],
+            ['...', 'Hello', 1],
+            ['', '', 5],
+        ];
+    }
+
+    /**
      * @dataProvider toWordsProvider
      */
     public function testToWords(
@@ -337,6 +360,375 @@ final class StrTest extends TestCase
             ['twoWords12', null, 'two_words12', 'two-words12', 'twoWords12', 'TwoWords12'],
             ['TwoWords12', null, 'two_words12', 'two-words12', 'twoWords12', 'TwoWords12'],
             ['TWOWords12', null, 'two_words12', 'two-words12', 'twoWords12', 'TwoWords12'],
+        ];
+    }
+
+    /**
+     * @dataProvider expandTabsProvider
+     */
+    public function testExpandTabs(
+        string $expected,
+        string $text,
+        int $tabSize = 8,
+        int $column = 1
+    ): void {
+        $this->assertSame(
+            $expected,
+            Str::expandTabs($text, $tabSize, $column)
+        );
+    }
+
+    /**
+     * @return array<array{string,string,2?:int,3?:int}>
+     */
+    public static function expandTabsProvider(): array
+    {
+        return [
+            ['', '', 4],
+            ["\n", "\n", 4],
+            ["\n\n", "\n\n", 4],
+            ["\n    ", "\n\t", 4],
+            ["\n    \n", "\n\t\n", 4],
+            ['    ', "\t", 4],
+            ['a   ', "a\t", 4],
+            ['abcdef  ', "abcdef\t", 4],
+            ['abc de  f       ', "abc\tde\tf\t\t", 4],
+            ['   ', "\t", 4, 2],
+            ['a ', "a\t", 4, 3],
+            ['abcdef   ', "abcdef\t", 4, 4],
+            ['abc   de  f       ', "abc\tde\tf\t\t", 4, 7],
+            ['        ', "\t", 8],
+            ['a       ', "a\t", 8],
+            ['abcdef  ', "abcdef\t", 8],
+            ['abc     de      f               ', "abc\tde\tf\t\t", 8],
+            ["   \nabc ", "\t\nabc\t", 4, 2],
+            [
+                <<<EOF
+                    abc de  f       g
+                1   23  4
+                EOF,
+                <<<EOF
+                \tabc\tde\tf\t\tg
+                1\t23\t4
+                EOF,
+                4,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider expandLeadingTabsProvider
+     */
+    public function testExpandLeadingTabs(
+        string $expected,
+        string $text,
+        int $tabSize = 8,
+        bool $preserveLine1 = false,
+        int $column = 1
+    ): void {
+        $this->assertSame(
+            $expected,
+            Str::expandLeadingTabs($text, $tabSize, $preserveLine1, $column)
+        );
+    }
+
+    /**
+     * @return array<array{string,string,2?:int,3?:bool,4?:int}>
+     */
+    public static function expandLeadingTabsProvider(): array
+    {
+        return [
+            ['', '', 4],
+            ["\n", "\n", 4],
+            ["\n\n", "\n\n", 4],
+            ["\n    ", "\n\t", 4],
+            ["\n    \n", "\n\t\n", 4],
+            ['    ', "\t", 4],
+            ["a\t", "a\t", 4],
+            ['    a', "\ta", 4],
+            ["    a\t", "\ta\t", 4],
+            ["abcdef\t", "abcdef\t", 4],
+            ["    abc\tde\tf\t\t", "\tabc\tde\tf\t\t", 4],
+            ['   ', "\t", 4, false, 2],
+            ['  a', "\ta", 4, false, 3],
+            [' abcdef', "\tabcdef", 4, false, 4],
+            ["\tabcdef", "\tabcdef", 4, true, 4],
+            ["  abc\tde\tf\t\t", "\tabc\tde\tf\t\t", 4, false, 7],
+            ['        ', "\t", 8],
+            ["a\t", "a\t", 8],
+            ['        a', "\ta", 8],
+            ["   \nabc\t", "\t\nabc\t", 4, false, 2],
+            ["   \n    abc\t", "\t\n\tabc\t", 4, false, 2],
+            [
+                <<<EOF
+                    abc\tde\tf\t\tg
+                1\t23\t4
+                EOF,
+                <<<EOF
+                \tabc\tde\tf\t\tg
+                1\t23\t4
+                EOF,
+                4,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider linesToListsProvider
+     *
+     * @param mixed ...$args
+     */
+    public function testLinesToLists(string $expected, ...$args): void
+    {
+        $this->assertSame($expected, Str::eolToNative(Str::mergeLists(...$args)));
+    }
+
+    /**
+     * @return array<string,string[]>
+     */
+    public static function linesToListsProvider(): array
+    {
+        $defaultRegex = (
+            new ReflectionParameter([Str::class, 'mergeLists'], 'regex')
+        )->getDefaultValue();
+
+        $input1 = <<<EOF
+            - Before lists
+
+            Section:
+            - d
+            Other section:
+            - <not a letter>
+            Without a subsequent list
+            Section:
+            - a
+            - b
+            Section:
+            - c
+            - b
+            - d
+            EOF;
+
+        $input2 = <<<EOF
+            - Before lists
+            📍 Section:
+            - list item
+            - another
+
+            Other section:
+            - item i
+            - item ii
+
+            - Standalone
+
+            Also standalone
+
+            Section:
+            - another
+            - and another
+            EOF;
+
+        $input3 = <<<EOF
+            ### Changes
+
+            - Description
+
+            - Description
+              over
+              multiple
+
+              ```
+              lines
+              ```
+
+            ### Changes
+
+            - Description
+              with different details
+
+            - Description
+              over
+              multiple
+
+              ```
+              lines
+              ```
+
+
+            EOF;
+
+        $input4 = <<<EOF
+            - Description
+            - Description
+              over
+              multiple
+
+              ```
+              lines
+              ```
+            - Description
+              with different details
+            - Description
+              over
+              multiple
+
+              ```
+              lines
+              ```
+            EOF;
+
+        return [
+            'Default' => [
+                <<<EOF
+                - Before lists
+                Without a subsequent list
+                Section:
+                - d
+                - a
+                - b
+                - c
+                Other section:
+                - <not a letter>
+                EOF,
+                $input1,
+            ],
+            'Markdown' => [
+                <<<EOF
+                - Before lists
+
+                Without a subsequent list
+
+                Section:
+
+                - d
+                - a
+                - b
+                - c
+
+                Other section:
+
+                - <not a letter>
+                EOF,
+                $input1,
+                "\n\n",
+            ],
+            'Nested' => [
+                <<<EOF
+                - Before lists
+
+                - Without a subsequent list
+
+                - Section:
+
+                  - d
+                  - a
+                  - b
+                  - c
+
+                - Other section:
+
+                  - <not a letter>
+                EOF,
+                $input1,
+                "\n\n",
+                '-',
+            ],
+            'Default (multibyte)' => [
+                <<<EOF
+                - Before lists
+                - Standalone
+                📍 Also standalone
+                📍 Section:
+                  - list item
+                  - another
+                  - and another
+                📍 Other section:
+                  - item i
+                  - item ii
+                EOF,
+                $input2,
+                "\n",
+                '📍',
+            ],
+            'Markdown (multibyte)' => [
+                <<<EOF
+                - Before lists
+                - Standalone
+
+                📍 Also standalone
+
+                📍 Section:
+
+                  - list item
+                  - another
+                  - and another
+
+                📍 Other section:
+
+                  - item i
+                  - item ii
+                EOF,
+                $input2,
+                "\n\n",
+                '📍',
+            ],
+            'Markdown (multiline #1, loose)' => [
+                <<<EOF
+                ### Changes
+
+                - Description
+                - Description
+                  over
+                  multiple
+
+                  ```
+                  lines
+                  ```
+                - Description
+                  with different details
+                EOF,
+                $input3,
+                "\n\n",
+                null,
+                $defaultRegex,
+                false,
+                true,
+            ],
+            'Markdown (multiline #1, not loose)' => [
+                <<<EOF
+                - Description
+                  over
+                  multiple
+
+                  ```
+                  lines
+                  ```
+
+                ### Changes
+
+                - Description
+                - Description
+                  with different details
+                EOF,
+                $input3,
+                "\n\n",
+            ],
+            'Markdown (multiline #2)' => [
+                <<<EOF
+                - Description
+                - Description
+                  over
+                  multiple
+
+                  ```
+                  lines
+                  ```
+                - Description
+                  with different details
+                EOF,
+                $input4,
+                "\n\n",
+            ],
         ];
     }
 }
