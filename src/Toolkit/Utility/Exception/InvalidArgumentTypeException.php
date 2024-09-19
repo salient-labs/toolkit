857be3be7267bef1392c6843199c8b0d145ced1b
Luke Arms <luke@linacreative.com>
2024-09-19 19:42:37 +1000

    Standardise string literal syntax in documentation

--- src/Toolkit/Utility/Exception/InvalidArgumentTypeException.php
+++ src/Toolkit/Utility/Exception/InvalidArgumentTypeException.php
@@ -15,7 +15,7 @@ class InvalidArgumentTypeException extends InvalidArgumentException
      *
      * @param int<1,max> $number Argument number (1-based).
      * @param string $name Argument name (leading `'$'` removed if present).
-     * @param string $type Expected type, e.g. `'string[]|null'`.
+     * @param string $type Expected type, e.g. `"string[]|null"`.
      * @param mixed $value Value given.
      */
     public function __construct(int $number, string $name, string $type, $value)
