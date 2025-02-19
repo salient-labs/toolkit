47dfa79420656b52c924a8c95eff8dfbffa20195
Luke Arms <luke@linacreative.com>
2025-02-19 15:12:37 +1100

    PHPStan: Update `Arr::flatten()` return type extension
    - Preserve key types when `$preserveKeys = true` is passed to
      `Arr::flatten()`
    - Handle optional keys in arrays passed to `Arr::flatten()` by making
      them optional in the return type

--- tests/unit/Toolkit/PHPStan/Utility/Type/ArrFlattenReturnTypeExtensionAssertions.php
+++ tests/unit/Toolkit/PHPStan/Utility/Type/ArrFlattenReturnTypeExtensionAssertions.php
@@ -15,11 +15,31 @@ $b = [];
 $c = [];
 /** @var array{3:"foo",bar:"baz"} */
 $d = [];
+/** @var array<string,array<class-string,string>> */
+$e = [];
+/** @var array{array{foo?:string,bar:int,baz?:string},array{foo?:int,bar?:string,baz:int}} */
+$f = [];
+$g = [
+    'foo' => 0,
+    'bar' => 1,
+    'baz' => 2,
+    [
+        'foo' => null,
+        'bar' => null,
+        'baz' => [
+            'FOO' => 3,
+            'BAR' => 71,
+        ],
+    ],
+];
 
 assertType('array<mixed>', Arr::flatten());
 assertType('array<mixed>', Arr::flatten('foo'));
 assertType('array<mixed>', Arr::flatten([], 'foo'));
+assertType('array<mixed>', Arr::flatten([], 'foo', -1));
+assertType('array{}', Arr::flatten([]));
 assertType('array{1}', Arr::flatten([1]));
+assertType('array{2}', Arr::flatten([1 => 2]));
 assertType('array{1}', Arr::flatten([[1]]));
 assertType('array{1}', Arr::flatten([[[1]]]));
 assertType('array{array{array{1}}}', Arr::flatten([[[1]]], 0));
@@ -38,6 +58,10 @@ assertType("array{array{1, 'foo', array{null, true}}}", Arr::flatten([[[[1, 'foo
 assertType('array{}', Arr::flatten([[[[[]]]]]));
 assertType('array{array{array{array{}}}}', Arr::flatten([[[[[]]]]], 1));
 assertType('array{array{}}', Arr::flatten([[[[[]]]]], 3));
+assertType('array{}', Arr::flatten([], -1, true));
+assertType('array{1}', Arr::flatten([1], -1, true));
+assertType('array{1: 2}', Arr::flatten([1 => 2], -1, true));
+assertType('array{1: 2}', Arr::flatten([1 => 2], 0, true));
 assertType('array<array<string, array<array<int>>>>', Arr::flatten($a, 0));
 assertType('array<array<array<int>>>', Arr::flatten($a, 1));
 assertType('array<array<int>>', Arr::flatten($a, 2));
@@ -57,3 +81,15 @@ assertType("array{array<string, array<int, array{3: 'foo', bar: 'baz'}>>}", Arr:
 assertType("array<'baz'|'foo'>", Arr::flatten([$c]));
 assertType("array{'foo', 'baz'}", Arr::flatten($d, 0));
 assertType("array{'foo', 'baz'}", Arr::flatten($d));
+assertType("array{3: 'foo', bar: 'baz'}", Arr::flatten($d, 0, true));
+assertType("array{3: 'foo', bar: 'baz'}", Arr::flatten($d, -1, true));
+assertType('array<string>', Arr::flatten($e));
+assertType('array<class-string, string>', Arr::flatten($e, -1, true));
+assertType('array{0: int|string, 1: int|string, 2?: int|string, 3?: int|string, 4?: int|string, 5?: int}', Arr::flatten($f));
+assertType('array{foo?: int|string, bar: int|string, baz: int}', Arr::flatten($f, -1, true));
+assertType('array{0, 1, 2, null, null, 3, 71}', Arr::flatten($g));
+assertType('array{0, 1, 2, null, null, array{FOO: 3, BAR: 71}}', Arr::flatten($g, 1));
+assertType('array{foo: null, bar: null, baz: 2, FOO: 3, BAR: 71}', Arr::flatten($g, -1, true));
+assertType('array{foo: 0, bar: 1, baz: 2, 0: array{foo: null, bar: null, baz: array{FOO: 3, BAR: 71}}}', Arr::flatten($g, 0, true));
+assertType('array{foo: null, bar: null, baz: array{FOO: 3, BAR: 71}}', Arr::flatten($g, 1, true));
+assertType('array{foo: null, bar: null, baz: 2, FOO: 3, BAR: 71}', Arr::flatten($g, 2, true));
