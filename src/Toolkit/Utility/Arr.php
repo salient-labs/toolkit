a8f440eb6e65853975e219607d5bfe12a47c75eb
Luke Arms <luke@linacreative.com>
2025-02-19 14:48:59 +1100

    Utility: Optionally preserve keys in `Arr::pluck()` and `Arr::flatten()`

--- src/Toolkit/Utility/Arr.php
+++ src/Toolkit/Utility/Arr.php
@@ -25,18 +25,29 @@ final class Arr extends AbstractUtility
     /**
      * Get values from a list of arrays using dot notation
      *
-     * @param iterable<mixed[]> $array
-     * @return ($key is null ? list<mixed> : mixed[])
+     * @template TKey
+     *
+     * @param iterable<TKey,mixed[]> $array
+     * @param string|bool|null $key - `true`: keys in `$array` are preserved.
+     * - `false` or `null` (default): keys are discarded.
+     * - `string`: keys are replaced with values using dot notation.
+     * @return ($key is true ? (TKey is array-key ? array<TKey,mixed> : array<array-key,mixed>) : ($key is false|null ? list<mixed> : mixed[]))
      */
-    public static function pluck(iterable $array, string $value, ?string $key = null): array
+    public static function pluck(iterable $array, string $value, $key = null): array
     {
-        foreach ($array as $item) {
+        if ($key === false) {
+            $key = null;
+        }
+        foreach ($array as $itemKey => $item) {
             $itemValue = self::get($item, $value, null);
             if ($key === null) {
                 $plucked[] = $itemValue;
                 continue;
+            } elseif ($key === true) {
+                $itemKey = self::getKey($itemKey, $i);
+            } else {
+                $itemKey = self::get($item, $key);
             }
-            $itemKey = self::get($item, $key);
             $plucked[$itemKey] = $itemValue;
         }
         return $plucked ?? [];
@@ -989,19 +1000,31 @@ final class Arr extends AbstractUtility
      * for no limit.
      * @return mixed[]
      */
-    public static function flatten(iterable $array, int $limit = -1): array
-    {
+    public static function flatten(
+        iterable $array,
+        int $limit = -1,
+        bool $preserveKeys = false
+    ): array {
         do {
             $flattened = [];
+            $i = null;
             $fromIterable = false;
-            foreach ($array as $value) {
+            foreach ($array as $key => $value) {
                 if (!is_iterable($value) || !$limit) {
-                    $flattened[] = $value;
+                    if ($preserveKeys) {
+                        $flattened[self::getKey($key, $i)] = $value;
+                    } else {
+                        $flattened[] = $value;
+                    }
                     continue;
                 }
                 $fromIterable = true;
-                foreach ($value as $value) {
-                    $flattened[] = $value;
+                foreach ($value as $key => $value) {
+                    if ($preserveKeys && ($limit === 1 || !is_iterable($value))) {
+                        $flattened[self::getKey($key, $i)] = $value;
+                    } else {
+                        $flattened[] = $value;
+                    }
                 }
             }
             $limit--;
